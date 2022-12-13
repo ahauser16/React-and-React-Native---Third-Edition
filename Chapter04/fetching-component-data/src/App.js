f497df5570efd623b631b362d0c38ce3522b3337
Arthur Hauser <arthur.hauser@gmail.com>
2022-12-13 18:03:26 -0500

    reviewed fetching component data from chapter 4

--- Chapter04/fetching-component-data/src/App.js
+++ Chapter04/fetching-component-data/src/App.js
@@ -1,7 +1,9 @@
 import React, { Fragment, useEffect, useState } from "react";
 
 function fetchUser() {
-  return new Promise(resolve => {
+  //The fetchUser() function returns a PROMISE which resolves to be a simple object with two properties, id and name.
+  return new Promise((resolve) => {
+    //The setTimeout() function delays the promise resolution for 1 second so as to mimic asynchronous behavior just like a normal fetch() call would.
     setTimeout(() => {
       resolve({ id: 1, name: "Adam" });
     }, 1000);
@@ -9,11 +11,18 @@ function fetchUser() {
 }
 
 export default function App() {
+  //We declare our initial state for ID and NAME and the setter functions we will use to update that state.
   const [id, setId] = useState("loading...");
   const [name, setName] = useState("loading...");
 
+  //The useEffect() Hook is used to run "side-effects" within the component.  Meaning that functional components should have one job: return JSX content to render to the DOM.  So if the component needs to do something else (such as fetching API data) then that code should be placed (lexically) inside a useEffect() Hook such as in the example below.
+
+  //The useEffect() Hook expects a function as an argument which is called after the component finishes rendering in a safe way that does not interfere with anything else that React is doing with the component under the hood.
+
+  //After our state is defined the useEffect() function is used to set up an IIFE function that calls fetchUser() and sets the state of the App component WHEN the promise resolves.
+
   useEffect(() => {
-    fetchUser().then(user => {
+    fetchUser().then((user) => {
       setId(user.id);
       setName(user.name);
     });
--- Chapter04/fetching-component-data/src/index.js
+++ Chapter04/fetching-component-data/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//Upon the first render the app renders the initial state values for ID:"loading..." and NAME:"loading..."
+//After one second the setTimeout function resolves the PROMISE that contains the data from the API response which is then used to update the ID and NAME states.  This causes the entire App component to re-render.
+//IN PRACTICE (in the real world) there is a good chance that an internet user will navigate around the application while an API request is still pending so we can modify our useEffect() Hook to deal with canceling these requests.
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
