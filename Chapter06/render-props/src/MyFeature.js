ded81262725297550e850a85af4424ab81a034e1
Arthur Hauser <arthur.hauser@gmail.com>
2022-12-23 15:41:21 -0500

    add notes about refactoring a feature into smaller reusable components

--- Chapter06/render-props/src/MyFeature.js
+++ Chapter06/render-props/src/MyFeature.js
@@ -85,6 +85,10 @@ export default class MyFeature extends Component {
     });
   };
 
+  //The render()-props technique is an officially recognized (and popular) way to deal with dependency and subsitutition problems.  See https://reactjs.org/docs/render-props.html for more info--> The term “render-prop” refers to a technique for sharing code between React components using a prop whose value is a function.  A component with a render prop takes a function, that returns a React element, and calls it instead of implementing its own render logic.   
+
+  // By using the render()-props technique we can more easily substitute one component for another such as if we want to use MyFeature in different parts of our application where it makes sense to use a different implementation of the ArticleList or AddArticle components.
+
   render() {
     const { articles, title, summary } = this.state;
     const {
@@ -96,6 +100,8 @@ export default class MyFeature extends Component {
       onChangeSummary
     } = this;
 
+
+    //NB--> the addArticle() and articleList() functions are called with the same property values that would have been passed to <AddArticle> and <ArticleList> but now MyFeature.js no longer imports AddArticle or ArticleList as dependencies.
     return (
       <section>
         {addArticle({
--- Chapter06/render-props/src/index.js
+++ Chapter06/render-props/src/index.js
@@ -6,12 +6,13 @@ import MyFeature from "./MyFeature";
 
 render(
   <MyFeature
+    //We've refactored this code so that now this is where the addArticle and articleList render-props are passed to the MyFeature component.  These prop values are functions that accept argument values from MyFeature.
     addArticle={({
       title,
       summary,
       onChangeTitle,
       onChangeSummary,
-      onClickAdd
+      onClickAdd,
     }) => (
       <AddArticle
         name="Articles"
@@ -22,6 +23,7 @@ render(
         onClickAdd={onClickAdd}
       />
     )}
+    //For example, even though the onClickToggle() function is declared in (and used to change the state of) the MyFeature component we can still use the render prop function to pass the onClickToggle() function (and any other values) to the component that will be rendered which in this case is ArticleList.  Ultimately, the return values from the addArticle and articleList functions are what's rendered.
     articleList={({ articles, onClickToggle, onClickRemove }) => (
       <ArticleList
         articles={articles}
